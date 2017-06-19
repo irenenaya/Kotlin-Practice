9707006fa8a54c9958e56bb3d9e8b27c312dddd1
Irene Naya <irenayamackay@gmail.com>
2017-06-19 00:06:35 -0300

    quicksort modified

--- algorithm/quick.kt
+++ algorithm/quick.kt
@@ -5,45 +5,13 @@
 /**
  * Recursive implementation of quicksort that uses a random element as a pivot, instead
  * of using the first (or last) element.
- * It uses an extension function, partitionAroundPivot() that splits the list in 3 parts, with
- * the pivot in the center, so it works for any method used to choose pivot, and it works for lists with
+ * It works for any method used to choose pivot, and it works for lists with
  * repeated values.
  * It takes any Comparable type, that is any class that inherits from Comparable and has a compareTo()
  * defined.
  * It also works for Map and any other container that takes Pairs, as long as a comparison function is
- * passed to define the comparison.
- *
+ * passed.
  */
-
-/** ------------------------------------------------------------------------------------------------
- * Helper class and extension functions for partitioning around the pivot
- *------------------------------------------------------------------------------------------------ */
-private data class  Three<T>(var left : List<T>, var center : List<T>, var right : List<T>)
-
-private fun <T : Comparable<T>> List<T>.partitionAroundPivot(pivot : T) : Three<T>{
-    val ret = Three<T>(emptyList(), emptyList(), emptyList())
-    for (i in this) {
-        if (i < pivot) ret.left += i
-        else if (i > pivot) ret.right += i
-        else ret.center +=i
-    }
-    return ret
-}
-
-// Overloaded partition function that takes a predicate. The predicate must return a negative value, zero,
-// or a positive value, depending on the sorting order
-private fun <T  ,U> List<Pair<T, U>>
-        .partitionAroundPivot(pivot : Pair<T, U>, comp : ((x : Pair<T,U>, y : Pair<T,U>) -> Int))
-        : Three<Pair<T,U>>{
-    val ret = Three<Pair<T,U>>(emptyList(), emptyList(), emptyList())
-    for (i in this) {
-        if (comp(i, pivot) < 0) ret.left += i
-        else if (comp(i, pivot) > 0) ret.right += i
-        else ret.center +=i
-    }
-    return ret
-}
-/*-------------------------------------------------------------------------------------------------*/
 
 /**-------------------------------------------------------------------------------------------------
  * Quicksort overloaded functions. The first function takes a List<T>, the second one takes a List
@@ -54,24 +22,24 @@ fun <T: Comparable<T>> quickSortRec(data : List<T>) : List<T> = when  {
     data.size < 2 -> data
     else -> {
         val pivot  = data[(Math.random() * data.size ).toInt()]
-        val (left, center, right) = data.partitionAroundPivot( pivot)
-        quickSortRec(left) + center + quickSortRec(right)
+        val (left, right) = (data - pivot).partition( { it < pivot})
+        quickSortRec(left) + pivot + quickSortRec(right)
     }
 }
 
-fun <T,U> quickSortRec(data : List<Pair<T,U>>, comp : ( (x : Pair<T,U>, y : Pair<T,U>) -> Int))
+fun <T,U> quickSortRec(data : List<Pair<T,U>>, comp : ( (x : Pair<T,U>, y : Pair<T,U>) -> Boolean))
         : List<Pair<T,U>> =
         when {
             data.size < 2 -> data
             else -> {
                 val pivot = data[(Math.random() * data.size).toInt()]
-                val (left, center, right) = data.partitionAroundPivot(pivot, comp)
-                quickSortRec(left, comp) + center + quickSortRec(right, comp)
+                val (left, right) = (data - pivot).partition( { comp(it,pivot)})
+                quickSortRec(left, comp) + pivot + quickSortRec(right, comp)
             }
         }
 
 fun <T : Comparable<T>> quickSortRec(data : Set<T>) = quickSortRec(data.toList()).toSet()
-fun <T, U> quickSortRec(data : Map<T, U>, comp : ( (x : Pair<T,U>, y : Pair<T,U>) -> Int)) =
+fun <T, U> quickSortRec(data : Map<T, U>, comp : ( (x : Pair<T,U>, y : Pair<T,U>) -> Boolean)) =
         quickSortRec(data.toList(), comp).toMap()
 
 /*------------------------------------------------------------------------------------------------*/
@@ -87,24 +55,31 @@ data class Person(val name: String, val age: Int, val height: Double) : Comparab
 }
 
 fun main (vararg args : String) {
+
+    val x  = listOf(2,3,4,2)
+
+     val x1 = quickSortRec(x)
+    println("List of Ints")
+    for (i in x1) println(i)
+
     val xs = mutableListOf("hello", "Nice",  "of", "the", "earth",  "says", "hi", "to", "all", "of", "you")
     val xx = quickSortRec(xs)
-    println("Mutable List of strings")
+    println("\nMutable List of strings")
     for (y in xx) println(y)
 
     // custom object
     val p1 = Person("john", 43, 1.5)
     val q1 = Person("mary", 22, 1.65)
-    val px = listOf(p1, q1)
+    val px = setOf(p1, q1)
     val pw = quickSortRec(px)
-    println("\nList of Custom Objects")
+    println("\nSet of Custom Objects")
     for (p in pw) println("${p.name}, ${p.age}, ${p.height}")
 
     // Map
     val ma = mapOf(Pair("a", 3.2), Pair("c", 4.0), Pair("d", 1.1), Pair("e", 0.5), Pair("a", 1.2))
     val mx = quickSortRec(ma,
             { x : Pair<String,Double>, y : Pair<String,Double> ->
-                if (x.first < y. first) -1 else if (x.first > y.first) 1 else 0})
+                x.first < y.first})
     println("\nMap: ")
     for (m in mx) println("${m.key}, ${m.value}")
 
@@ -113,6 +88,6 @@ fun main (vararg args : String) {
     val li = listOf(Pair("a", 2.2), Pair("D", 4.5))
     val ys = quickSortRec(li,
             { x : Pair<String,Double>, y : Pair<String,Double> ->
-                if (x.second < y. second) -1 else if (x.second > y.second) 1 else 0})
+                 x.second < y.second})
     for (y in ys) println("${y.first} , ${y.second}")
 }
